7a94043d52443ae5847ab206cd8728f40e7c451c
Katelyn Mehner <kmehner@gmail.com>
2022-04-11 10:34:34 -0600

    Added Nav Bar and fixed root Dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-function App() {
-  return <h1>Hello World</h1>
+import React from "react"; // . fragment 
+import Nav from "./components/Nav";
+
+
+function App() { // Declarative statement <> </>
+  return(
+    <>  
+      <Nav></Nav>
+      <h1>Hello World</h1>
+    </>
+  );
 }
 
 export default App;
